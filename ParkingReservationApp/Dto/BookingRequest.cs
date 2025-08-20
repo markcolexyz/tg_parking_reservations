54932f5337380bbfe78c416b76874769100ed8a7
Mark Cole <markcolexyz@gmail.com>
2025-08-20 20:14:56 +0100

    Logic added for booking a slot

---
+++ ParkingReservationApp/Dto/BookingRequest.cs
@@ -0,0 +1,7 @@
+public class BookingRequest
+{
+    public int BookeeId { get; set; }
+    public int ParkingSpaceId { get; set; }
+    public int ParkingStructureId { get; set; }
+    public DateTime DateOfBooking { get; set; }
+}
