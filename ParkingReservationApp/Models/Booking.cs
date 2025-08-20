c133bc17a68582003affb07baa2efa3882dcab0e
Mark Cole <markcolexyz@gmail.com>
2025-08-20 16:34:38 +0100

    Explicitly map table names in the db to the models

--- ParkingReservationApp/Models/Booking.cs
+++ ParkingReservationApp/Models/Booking.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Booking
 {
+    [Column("bookingid")]
     public int BookingId { get; set; }
+    [Column("bookee_id")]
     public int BookeeId { get; set; }
+    [Column("dateofbooking")]
     public DateTime DateOfBooking { get; set; }
+    [Column("parkingspace_id")]
     public int ParkingSpaceId { get; set; }
+    [Column("parkingstructure_id")]
     public int ParkingStructureId { get; set; }
 
     public Bookee? Bookee { get; set; }
--- ParkingReservationApp/Models/Contact.cs
+++ ParkingReservationApp/Models/Contact.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 public class Contact
 {
+    [Column("contactid")]
     public int ContactId { get; set; }
+    [Column("firstname")]
     public string? Firstname { get; set; }
+    [Column("lastname")]
     public string? Lastname { get; set; }
+    [Column("contactinformation_id")]
     public int ContactInformationId { get; set; }
 }
